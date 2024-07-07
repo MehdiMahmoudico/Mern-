97c915a2bfa159c4a89af70335ad31e8912a9298
MehdiMahmoudico <mmeehdi.mma@gmail.com>
2024-07-07 12:24:39 +0100

    js

---
+++ JavaScript/Practice/Hoist/script.js
@@ -0,0 +1,83 @@
+// console.log(hello);                                   
+// var hello = 'world';                                 
+// // error message
+
+// console.log(hello);                                   
+// var hello = 'world';                                 
+
+// // Undefined
+
+// var needle = 'haystack';
+// test();
+// function test(){
+//     var needle = 'magnet';
+//     console.log(needle);
+// }
+
+
+// // magnet
+
+
+// var brendan = 'super cool';
+// function print(){
+//     brendan = 'only okay';
+//     console.log(brendan);
+// }
+// console.log(brendan);
+
+// // super cool
+
+// mean();
+// console.log(food);
+// var mean = function() {
+//     food = "chicken";
+//     console.log(food);
+//     var food = "fish";
+//     console.log(food);
+// }
+// console.log(food);
+
+// nothing
+
+// console.log(genre);
+// var genre = "disco";
+// rewind();
+// function rewind() {
+//     genre = "rock";
+//     console.log(genre);
+//     var genre = "r&b";
+//     console.log(genre);
+// }
+// console.log(genre);
+
+// undefined / rock /r&b / disco
+
+// dojo = "san jose";
+// console.log(dojo);
+// learn();
+// function learn() {
+//     dojo = "seattle";
+//     console.log(dojo);
+//     var dojo = "burbank";
+//     console.log(dojo);
+// }
+// console.log(dojo);
+
+// san jose / seattle / burbank / san jose
+
+// console.log(makeDojo("Chicago", 65));
+// console.log(makeDojo("Berkeley", 0));
+// function makeDojo(name, students){
+//     const dojo = {};
+//     dojo.name = name;
+//     dojo.students = students;
+//     if(dojo.students > 50){
+//         dojo.hiring = true;
+//     }
+//     else if(dojo.students <= 0){
+//         dojo = "closed for now";
+//     }
+//     return dojo;
+// }
+
+// true / closed for now if const was let but since its const is gona be a error
