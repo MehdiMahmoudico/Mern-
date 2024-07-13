8e6743a57792e4c47e760092b30e63f970dd316a
MehdiMahmoudico <mmeehdi.mma@gmail.com>
2024-07-13 15:15:55 +0100

    sensai

--- JavaScript/Practice/Ninja/script.js
+++ JavaScript/Practice/Ninja/script.js
@@ -1,9 +1,9 @@
 class Ninja{
-    constructor(name){
+    constructor(name,health=90,speed=3,strength=3){
         this.name = name;
-        this.health = 90;
-        this.speed = 3;
-        this.strength = 3;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
     }
     sayName(){
         console.log(`Ninja's name is ${this.name}`);
@@ -23,4 +23,21 @@ ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
 
+class Sensai extends Ninja{
+    constructor(name){
+        super(name,200,10,10)
+        this.wisdom = 10;
+    }
+    speakWisdom(){
+        super.drinkSake()
+        console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+    showStats(){
+        super.showStats();
+    }
+}
 
+
+const superSensei = new Sensai("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
